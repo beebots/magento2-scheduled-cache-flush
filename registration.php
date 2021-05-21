c1f4502f04ed22ee3ae6b4baddd1b2025313852d
Scott Friend <scottn@banyanbotanicals.com>
2021-05-21 09:47:03 -0700

    Initial commit. Mage module with no functionality

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'BeeBots_ScheduledCacheFlush',
+    __DIR__
+);
