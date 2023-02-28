266a88e7a8caeb989f21b76aba2c46414b007e6e
Scott Friend <scottn@banyanbotanicals.com>
2023-02-27 23:01:51 -0800

    Adding scheduled_cache_flush table and admin management page

---
+++ Controller/Adminhtml/Manage/Index.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace BeeBots\ScheduledCacheFlush\Controller\Adminhtml\Manage;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Index of Scheduled Cache Flush Records
+ */
+class Index extends Action
+{
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Index action
+     *
+     * @return ResultInterface
+     */
+    public function execute(): ResultInterface
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend(__("Scheduled Cache Management"));
+        return $resultPage;
+    }
+}
