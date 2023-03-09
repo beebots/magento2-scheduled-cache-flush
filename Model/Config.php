567b609a8a7f3f3732f98d6d9eabc69f901dbfc7
Scott Friend <scottn@banyanbotanicals.com>
2023-03-09 08:37:13 -0800

    Removing old now unused config

--- Model/Config.php
+++ Model/Config.php
@@ -2,44 +2,26 @@
 
 namespace BeeBots\ScheduledCacheFlush\Model;
 
-use Magento\Config\Model\ResourceModel\Config as ConfigResource;
-use Magento\Framework\App\Config\ReinitableConfigInterface;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 
 /**
- * Class Config
- *
- * @package BeeBots\ScheduledCacheFlush\Model
+ * Scheduled Cache Flush Configuration Class
  */
 class Config
 {
-    const ENABLED_PATH = 'beebots/scheduled_cache_flush/enabled';
-    const FLUSH_TIMES_PATH = 'beebots/scheduled_cache_flush/flush_times';
+    public const ENABLED_PATH = 'beebots/scheduled_cache_flush/enabled';
 
-    /** @var ScopeConfigInterface */
-    private $scopeConfig;
-
-    /** @var ReinitableConfigInterface */
-    private $reinitableConfig;
-
-    /** @var ConfigResource */
-    private $configResource;
+    private ScopeConfigInterface $scopeConfig;
 
     /**
      * Config constructor.
      *
      * @param ScopeConfigInterface $scopeConfig
-     * @param ReinitableConfigInterface $reinitableConfig
-     * @param ConfigResource $configResource
      */
     public function __construct(
         ScopeConfigInterface $scopeConfig,
-        ReinitableConfigInterface $reinitableConfig,
-        ConfigResource $configResource
     ) {
         $this->scopeConfig = $scopeConfig;
-        $this->reinitableConfig = $reinitableConfig;
-        $this->configResource = $configResource;
     }
 
     /**
@@ -49,33 +31,6 @@ class Config
      */
     public function isEnabled(): bool
     {
-        return (bool)$this->scopeConfig->isSetFlag(self::ENABLED_PATH);
-    }
-
-    /**
-     * Function: getFlushTimes
-     *
-     * @return string
-     */
-    public function getFlushTimes(): string
-    {
-        return (string)$this->reinitableConfig->getValue(self::FLUSH_TIMES_PATH);
-    }
-
-    /**
-     * Function: setFlushTimes
-     *
-     * @param string $flushTimes
-     *
-     * @return Config
-     */
-    public function setFlushTimes(string $flushTimes): Config
-    {
-        $this->configResource->saveConfig(
-            self::FLUSH_TIMES_PATH,
-            $flushTimes
-        );
-
-        return $this;
+        return $this->scopeConfig->isSetFlag(self::ENABLED_PATH);
     }
 }
